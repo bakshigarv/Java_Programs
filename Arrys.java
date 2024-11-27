c3a38392f2ead1d43940fadf4bc0b628f13c3608
bakshigarv <garvbakshi.qa@gmail.com>
2024-11-27 18:59:36 +0530

    new programs

---
+++ Arrys.java
@@ -0,0 +1,19 @@
+public class Arrys {
+    public static void main(String[] args) {
+        String[] cars = {
+                "Volvo", "BMW", "Ford", "Mazda"
+        };
+
+        // The code below is also a format to create an array:
+
+        /*
+        String[] cars = new String[4];
+        cars[0] = "Volvo";
+        cars[1] = "BMW";
+        cars[2] = "Ford";
+        cars[3] = "Mazda";
+        */
+
+        System.out.println(cars[0]);
+    }
+}
---
+++ Multiarray.java
@@ -0,0 +1,14 @@
+public class Multiarray {
+    public static void main(String[]args) {
+
+        String[][] cars = {
+            {
+            "TATA", "Hyundai", "Honda", "Mahindra"
+         },
+         {
+            "Mercedes", "BMW", "Jaguar", "Porche"
+        }
+        };
+        System.out.println(cars [1] [3]);
+    }
+}
